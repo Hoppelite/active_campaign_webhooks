44133067d8ab1f68a4b7740b45b8ae77718541f8
Sam <sam@banqer.co>
2020-10-02 14:19:18 +1300

    Break up modules

--- lib/active_campaign/webhooks/version.rb
+++ lib/active_campaign/webhooks/version.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-module ActiveCampaign::Webhooks
-  VERSION = '0.1.0'
+module ActiveCampaign
+  module Webhooks
+    VERSION = '0.1.0'
+  end
 end
--- lib/active_campaign_webhooks.rb
+++ lib/active_campaign_webhooks.rb
@@ -2,9 +2,11 @@
 
 require 'active_campaign/webhooks/version'
 
-module ActiveCampaign::Webhooks
-  Dir['active_campaign/webhooks/request/*.rb'].each do |f|
-    model = f.split('/').last.split('.rb').first
-    require model unless defined?(model)
+module ActiveCampaign
+  module Webhooks
+    Dir['active_campaign/webhooks/request/*.rb'].each do |f|
+      model = f.split('/').last.split('.rb').first
+      require model unless defined?(model)
+    end
   end
 end

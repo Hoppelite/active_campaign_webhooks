18a6c7ef7296be0926de0d5cc77ff150b82f0870
Sam <sam@banqer.co>
2020-10-05 12:11:14 +1300

    Test Again

--- lib/active_campaign/webhooks/version.rb
+++ lib/active_campaign/webhooks/version.rb
@@ -2,6 +2,6 @@
 
 module ActiveCampaign
   module Webhooks
-    VERSION = '0.1.14'
+    VERSION = '0.1.15'
   end
 end
--- lib/active_campaign_webhooks.rb
+++ lib/active_campaign_webhooks.rb
@@ -5,11 +5,10 @@ require 'active_campaign/webhooks/version'
 module ActiveCampaign
   module Webhooks
     module Request
-      puts "#{File.join(File.dirname(__FILE__))}/active_campaign/webhooks/request/*.rb"
       Dir["#{File.join(File.dirname(__FILE__))}/active_campaign/webhooks/request/*.rb"].sort.each do |f|
-        puts f
         file_path = f.split('.').first
         class_name = file_path.split('/').last.split('_').collect(&:capitalize).join
+        puts "Autoload: #{class_name}", file_path
         autoload class_name.to_sym, file_path
       end
     end

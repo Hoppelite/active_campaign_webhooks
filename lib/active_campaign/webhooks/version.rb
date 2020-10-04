100d086b322d3229a7003780f14742bdeb1ad37c
Sam <sam@banqer.co>
2020-10-05 11:58:54 +1300

    Fix autoloader

--- lib/active_campaign/webhooks/version.rb
+++ lib/active_campaign/webhooks/version.rb
@@ -2,6 +2,6 @@
 
 module ActiveCampaign
   module Webhooks
-    VERSION = '0.1.11'
+    VERSION = '0.1.12'
   end
 end
--- lib/active_campaign_webhooks.rb
+++ lib/active_campaign_webhooks.rb
@@ -6,7 +6,7 @@ module ActiveCampaign
   module Webhooks
     module Request
       Dir["#{File.join(File.dirname(__FILE__))}/active_campaign/webhooks/request/*.rb"].sort.each do |f|
-        file_path = f.split('.').first
+        file_path = f.split('/', 2).last.split('.').first
         class_name = file_path.split('/').last.split('_').collect(&:capitalize).join
         autoload class_name, file_path
       end

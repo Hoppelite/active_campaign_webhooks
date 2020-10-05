97e06ccde0d61530882641bb64cc53ad68f35227
Sam <sam@banqer.co>
2020-10-05 16:34:55 +1300

    Change const get in init to get from module

--- lib/active_campaign/webhooks/request/base_request.rb
+++ lib/active_campaign/webhooks/request/base_request.rb
@@ -23,7 +23,7 @@ module ActiveCampaign
         def initialize(hash = {})
           hash.each do |k, v|
             unless self.class.attribute_map[k].nil?
-              class_obj = Object.const_get(self.class.attribute_map[k])
+              class_obj = ActiveCampaign::Webhooks::Request.const_get(self.class.attribute_map[k])
               v = class_obj.new(v)
             end
             public_send("#{k}=", v) if respond_to? "#{k}=" 

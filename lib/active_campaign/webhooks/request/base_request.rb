c5c66951f06123680851eed49f57ae17883273cc
Sam <sam@banqer.co>
2020-10-05 16:28:26 +1300

    Ensure attribute map is initialized - Fix constant get for request init

--- lib/active_campaign/webhooks/request/base_request.rb
+++ lib/active_campaign/webhooks/request/base_request.rb
@@ -10,19 +10,20 @@ module ActiveCampaign
 
           def attribute name, classname = nil
             attr_accessor name
+            self.attribute_map = {} if self.attribute_map.nil?
             self.attribute_map[name] = classname unless classname.nil?
           end
         end
 
         self.attribute_map = {}
-        
+
         # @return [String]
         attribute :type
 
         def initialize(hash = {})
           hash.each do |k, v|
             unless self.class.attribute_map[k].nil?
-              class_obj = Object.const_get(k)
+              class_obj = Object.const_get(self.class.attribute_map[k])
               v = class_obj.new(v)
             end
             public_send("#{k}=", v) if respond_to? "#{k}=" 
